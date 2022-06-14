0008f55cbc82db278e0cc4603bf2912cf6bc97c3
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-06-14 21:59:29 +0800

    [Earl] used the getImages service on app.js with useEffect and useState hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { getImages } from "./utils/services";
 
 function App() {
-  return <div className=''></div>;
+	const [page, setPage] = useState(1);
+
+	useEffect(() => {
+		const getData = async () => {
+			const images = await getImages(page);
+			console.log(images);
+		};
+		//executes the function above
+		getData();
+	}, [page]);
+	return <div className='text-blue-500'>hello</div>;
 }
 
 export default App;
