4b3aea1ba27e2f1376636b781ab7c3bed068a34a
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-06-14 22:36:13 +0800

    [Earl] Imported ImageGrid to app.js and added new state for the images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 /** @format */
-
-import "./App.css";
 import { useEffect, useState } from "react";
+import ImageGrid from "./components/ImageGrid/ImageGrid";
 import { getImages } from "./utils/services";
 
 function App() {
 	const [page, setPage] = useState(1);
+	const [images, setImages] = useState([]);
 
 	useEffect(() => {
 		const getData = async () => {
-			const images = await getImages(page);
-			console.log(images);
+			const imagesData = await getImages(page);
+			setImages(imagesData);
 		};
 		//executes the function above
 		getData();
 	}, [page]);
-	return <div className='text-blue-500'>hello</div>;
+	return (
+		<div className='text-white w-full bg-sky-500 h-screen flex justify-center items-center'>
+			<ImageGrid imageArray={images} />
+		</div>
+	);
 }
 
 export default App;
