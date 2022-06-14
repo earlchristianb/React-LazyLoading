9118819a2a44797cfffd232d5143087e3309685f
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-06-14 22:39:50 +0800

    [Earl] Installed a dependency: react-spinner-animated

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const ImageGrid = ({ imageArray }) => {
 	return (
-		<div className='grid grid-cols-2 justify-items-stretch  gap-4 bg-red-300 w-full h-5/6 md:w-4/6 overflow-auto scroll-smooth scroll-p-3'>
+		<div className='grid grid-cols-2 justify-items-stretch  gap-4 bg-red-300 w-full h-5/6 md:w-4/6 overflow-auto scroll-smooth'>
 			{imageArray.map((image, index) => (
 				<div
 					className='border border-black flex justify-center items-center h-32'
