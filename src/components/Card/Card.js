68cb2c14da012b8dbc361a6a764311a423f81584
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-06-15 03:14:34 +0800

    [Earl] Added 10px rootmargin for the observer, unobserve the target once it enters the viewport

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,16 +13,17 @@ const Card = ({ author, download_url, isLast, nextPage }) => {
 				if (isLast && entry.isIntersecting) {
 					console.log(entry.isIntersecting);
 					nextPage();
+					obs.unobserve(entry.target);
 				}
 			},
-			{ threshold: 1 }
+			{ rootMargin: "10px" }
 		);
 		obs.observe(cardRef.current);
 	}, [cardRef, isLast, nextPage]);
 
 	return (
 		<div
-			className='flex flex-col justify-center items-center w-full min-h-60 bg-white rounded '
+			className='flex flex-col justify-center items-center w-full min-h-60 bg-white rounded mr-3 '
 			ref={cardRef}>
 			<div className='relative p-2  '>
 				<img alt='' className=' h-60 w-60 rounded ' src={download_url} />
